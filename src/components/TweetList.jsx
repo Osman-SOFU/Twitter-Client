89bc2f0ada9fbc0ebd3050b976c5dfc1f784e85b
Osman-SOFU <osofu1903@gmail.com>
2025-03-12 05:16:09 +0300

    React uygulamamı GitHub'a ekledim

---
+++ src/components/TweetList.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const TweetList = () => {
+  const [tweets, setTweets] = useState([]);
+  const userId = 1;
+
+  useEffect(() => {
+    // Basic Auth bilgileri
+    const username = "test@example.com"; // Kullanıcı adını gir
+    const password = "1234"; // Şifreni gir
+    const authHeader = "Basic " + btoa(`${username}:${password}`); // Base64 şifreleme
+
+    axios
+      .get(`http://localhost:3000/tweet/findByUserId?userId=${userId}`, {
+        headers: {
+          Authorization: authHeader,
+          "Content-Type": "application/json",
+        },
+        withCredentials: true, // Kimlik doğrulama bilgilerini ekler
+      })
+      .then((response) => {
+        console.log("Tweetler:", response.data);
+        setTweets(response.data);
+      })
+      .catch((error) => {
+        console.error("Tweetleri çekerken hata oluştu:", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>Kullanıcının Tweetleri</h2>
+      {tweets.length > 0 ? (
+        <ul>
+          {tweets.map((tweet) => (
+            <li key={tweet.id}>{tweet.text}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>Tweet bulunamadı.</p>
+      )}
+    </div>
+  );
+};
+
+export default TweetList;
